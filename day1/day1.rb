68e78113daaf8c40b6069ed5141c3e4c83e4f273
Jared Ledvina <jared@techsmix.net>
2019-12-09 22:54:24 -0500

    Day 1, Part 2, complete

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,6 +1,24 @@
 input = File.read("input").split
 
-part1 = input.map { |x| (x.to_i / 3) - 2 }.inject(0, :+)
+def calc_fuel(mass)
+  return mass.to_i / 3 -2
+end
 
-puts part1
+part1 = input.map { |x| calc_fuel(x) }.inject(0, :+)
+
+part2 ||= []
+input.each do |x|
+  fuel = calc_fuel(x)
+  part2 << fuel
+  while fuel >= 0
+    fuel = calc_fuel(fuel)
+    unless fuel <= 0
+        part2 << fuel
+    end
+  end
+end
+
+puts "Part 1: ", part1
+puts "Part 2: ", part2.inject(0, :+)
+
 
